ec58016052cdb71e7f56b92607ce56138d2c535a
Ruhshan <ruhshan.ahmed@gmail.com>
2018-07-22 23:57:58 +0600

    Tutorial 13

--- gotut.go
+++ gotut.go
@@ -8,31 +8,31 @@ import (
 )
 
 type SitemapIndex struct {
-	Locations []Location `xml:"sitemap"`
+	Locations []string `xml:"sitemap>loc"`
 }
 
-type Location struct {
-	Loc string `xml:"loc"`
-}
-
-func (l Location) String() string {
-	return fmt.Sprintf(l.Loc)
+type News struct {
+	Titeles []string `xml:"url>news>title"`
+	Keywords []string `xml:"url>mnews>Keywords"`
+	Locations []string `xml:"url>loc"`
 }
 
 func main() {
-
+	var s SitemapIndex
+	var n News
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
-
-	var s SitemapIndex
 
 	xml.Unmarshal(bytes, &s)
 
 	//fmt.Println(s.Locations)
 
 	for _, Location := range s.Locations{
-		fmt.Printf("\n%s",Location)
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		xml.Unmarshal(bytes, &n)
 	}
 
 }
