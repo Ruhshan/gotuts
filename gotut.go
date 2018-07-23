b4703dcd4506361c1e4d680826bc007e6ba88e76
Ruhshan <ruhshan.ahmed@gmail.com>
2018-07-23 17:27:29 +0600

    Tutorial 15

--- gotut.go
+++ gotut.go
@@ -1,27 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
+type SitemapIndex struct {
+	Locations []string `xml:"sitemap>loc"`
+}
+
+type News struct {
+	Titles []string `xml:"url>news>title"`
+	Keywords []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
+}
+
+type NewsMap struct {
+	Keyword string
+	Location string
+}
 
 func main() {
-	grades := make(map[string]float32)
+	var s SitemapIndex
+	var n News
+	news_map := make(map[string]NewsMap)
 
-	grades["tim"] = 42
-	grades["monty"] = 92
-	grades["sam"] = 67
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
 
+	xml.Unmarshal(bytes, &s)
 
-	TimsGrade := grades["tim"]
-	
-	fmt.Println(TimsGrade)
+	//fmt.Println(s.Locations)
 
-	delete(grades, "tim")
-	fmt.Println(grades)
+	for _, Location := range s.Locations{
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		xml.Unmarshal(bytes, &n)
 
-	for k, v := range grades {
-		fmt.Println(k,":",v)
+		for idx, _ := range n.Titles {
+			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		}
 	}
 
-
+	for idx, data := range news_map{
+		fmt.Println("Index: \n\n", idx)
+		fmt.Println("Keywords:\n", data.Keyword)
+		fmt.Println("Locations:\n", data.Location)
+	}
 
 }
