9039c51ea3518807c148ccb013f92cb8d4799ad2
Ruhshan <ruhshan.ahmed@gmail.com>
2018-07-26 14:18:10 +0600

    Tutorial 23

--- gotut.go
+++ gotut.go
@@ -1,21 +1,30 @@
 package main
 
-import "fmt"
-	
-func foo(c chan int, someValue int){
+import ("fmt"
+		"sync")
 
+var wg sync.WaitGroup
+
+func foo(c chan int, someValue int){
+	defer wg.Done()
 	c <- someValue * 5
 }
 
 func main() {
-	fooVal := make(chan int)
-	go foo(fooVal, 5)
-	go foo(fooVal, 3)
+	fooVal := make(chan int, 10)
+	
+	for i:=0;i<10;i++{
+		wg.Add(1)
+		go foo(fooVal,i)
+	}
 
-	v1 := <- fooVal
-	v2 := <- fooVal
+	wg.Wait()
 
-	fmt.Println(v1,v2)
+	close(fooVal)
+
+	for item := range fooVal{
+		fmt.Println(item)
+	}
 
 
 }
